921333ad53b6c0e3525963ae96828471b3bd3411
Krzysztof Nazarewski <nazarewk@gmail.com>
2016-02-19 11:13:44 +0100

    blog entry

---
+++ src/app/dijit/Entry.js
@@ -0,0 +1,28 @@
+define([
+  '../../bower_components/dojo/_base/declare',
+  'dijit/_WidgetBase',
+  'dojo/dom-construct',
+  'dojo/on'
+], function (declare, _WidgetBase, domConstruct, on) {
+  return declare([_WidgetBase], {
+
+    buildRendering: function buildRendering() {
+      // Normally this.domNode is a div created in super() call
+      // this.inherited(arguments) in dojo's inheritance model
+      // we'll skip it and create <li> instead
+      this.domNode = domConstruct.create('li', {
+        innerHTML: this.item.title
+      });
+    },
+
+    postCreate: function postCreate() {
+      this.inherited(arguments);
+      this.signal = on(this.domNode, 'click', this.item.onClick);
+    },
+
+    destroy: function destroy() {
+      this.signal.remove();
+      this.inherited(arguments);
+    }
+  });
+});
